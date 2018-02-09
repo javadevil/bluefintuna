4e160037ec4c6cdba9203c210b9a043b8ae4619c
Wacharabuhm Tungketmukda <n.kimmy@gmail.com>
2018-02-09 16:03:49 +0700

    save

--- cryptopia.py
+++ cryptopia.py
@@ -13,14 +13,44 @@ import base64
 import json
 
 from exchanges import Exchange
+from exchanges import TradePair
+from exchanges import Currency
 
 API_KEY = ""
 API_SECRET = ""
 
 class Cryptopia(Exchange):
     def init_currency(self):
-        api_query("GetBalance")  
-
+        super().init_currency()
+        currencies_data = api_query("GetCurrencies")["Data"]
+        for c in currencies_data:
+            self.currencies[c["Symbol"]] = Currency(c["Symbol"], c["Name"], withdraw_fee=c["WithdrawFee"])
+    
+    def init_tradepairs(self):
+        super().init_tradepairs()
+        tradepairs_data = api_query("GetTradePairs")["Data"]
+        for p in tradepairs_data:
+            if self.currencies.get(p["Symbol"]) and self.currencies.get(p["BaseSymbol"]):
+                self.tradepairs[p["Label"]] = TradePair(self.currencies.get(p["Symbol"]), self.currencies.get(p["BaseSymbol"]), p["TradeFee"])
+    
+    def update_currency(self):
+        balances_data = api_query("GetBalance")["Data"]
+        for b in balances_data:
+            balance = self.currencies.get(b["Symbol"])
+            balance.balance = b["Available"]
+            balance.deposit_pending = b["Unconfirmed"]
+            balance.withdraw_pending = b["PendingWithdraw"]
+            balance.trade_held = b["HeldForTrades"]
+    
+    def update_tradepairs(self):
+        markets_data = api_query("GetMarkets")["Data"]
+        for m in markets_data:
+            tradepair = self.tradepairs.get(m["Label"])
+            if not tradepair == None:
+                tradepair.bids = m["BidPrice"]
+                tradepair.asks = m["AskPrice"]
+        print(self.tradepairs["NMC/BTC"])
+        
 def api_query( method, req = None ):
     if not req:
         req = {}
@@ -33,7 +63,7 @@ def api_query( method, req = None ):
         if req:
             for param in req:
                 url += '/' + str( param )
-            r = requests.get( url )
+        r = requests.get( url )
     elif method in private_set:
         url = "https://www.cryptopia.co.nz/Api/" + method
         nonce = str( int( time.time() ) )
@@ -47,9 +77,13 @@ def api_query( method, req = None ):
         headers = { 'Authorization': header_value, 'Content-Type':'application/json; charset=utf-8' }
         r = requests.post( url, data = post_data, headers = headers )
     response = r.text
-    print("( Response ): " + response)
+    #print("( Response ): " + response)
     return json.loads(response)
     
 
         
 c = Cryptopia()
+c.update()
+while True:
+    time.sleep(15)
+    c.update_tradepairs()
--- exchanges.py
+++ exchanges.py
@@ -11,10 +11,10 @@ class Exchange():
         self.init_tradepairs()
 
     def init_currency(self):
-        pass
+        self.currencies = dict()
     
     def init_tradepairs(self):
-        pass
+        self.tradepairs = dict()
     
     def update(self):
         self.update_currency()
@@ -31,21 +31,27 @@ class TradePair():
         self.secondary_currency = secondary_currency
         self.primary_currency = primary_currency
         self.trade_fee = trade_fee
-        
+        self.bids = 0.0
+        self.asks = 0.0
+    
+    def __str__(self):
+        return "{0}/{1}\t Bids:{2:5.8f} Asks:{3:5.8f} TF:{4:5.8f}".format(self.secondary_currency.symbol, self.primary_currency.symbol, self.bids, self.asks, self.trade_fee)
+    
 class Currency():
-    def __init__(self, symbol, name, withdraw_fee=0.0):
+    def __init__(self, symbol, name, address=None, withdraw_fee=0.0):
         self.symbol = symbol
         self.name = name
+        self.address = address
         self.withdraw_fee = withdraw_fee
-        
-    def update_balance(self):
         self.balance = 0.0
         self.deopsit_pending = 0.0
         self.withdraw_pending = 0.0
-        self.trade_order = 0.0
+        self.trade_held = 0.0
 
+    def total(self):
+        return self.balance + self.deopsit_pending + self.withdraw_pending + self.trade_held
     def __eq__(self, other):
         return self.symbol == other.symbol
     
     def __str__(self):
-        return "{0} ({1})".format(self.symbol, self.name)
+        return "{0}\t Bal:{1:5.8f} DP:{2:5.8F} WP:{3:5.8f} TH:{4:5.8f} WF:{5:5.8f}".format(self.symbol, self.balance, self.deopsit_pending, self.withdraw_pending, self.trade_held, self.withdraw_fee)
